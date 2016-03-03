5c1b6d0ce1f9bd3d8c26268bdd88e0553f2d441e
Patrick Hergan <hergan.patrick@gmail.com>
2016-03-03 22:59:07 +0100

    change: it is not any longer possible to draw ECTS cards

--- UnityFiles(Andi)/SWEB/Assets/db/dbInterface.cs
+++ UnityFiles(Andi)/SWEB/Assets/db/dbInterface.cs
@@ -157,29 +157,33 @@ public class dbInterface {
 		int card_type = 0;
 		int card_id = 0;
 
-		IDbConnection dbconn;
-		dbconn = (IDbConnection) new SqliteConnection(conn);
-		dbconn.Open(); //Open connection to the database.
-		IDbCommand dbcmd = dbconn.CreateCommand();
+		do {
 
-		dbcmd.CommandText = sqlQuery;
-		IDataReader reader = dbcmd.ExecuteReader();
-		while(reader.Read())
-		{
-			card_id = reader.GetInt32(0);
-			card_name = reader.GetString(1);
-			card_image = reader.GetString(2);
-			card_type = reader.GetInt32(3);
+			IDbConnection dbconn;
+			dbconn = (IDbConnection)new SqliteConnection (conn);
+			dbconn.Open (); //Open connection to the database.
+			IDbCommand dbcmd = dbconn.CreateCommand ();
 
-			Debug.Log( "card_id=" + card_id + " card_name=" + card_name + " card_image=" + card_image + " card_type=" + card_type);
-		}
+			dbcmd.CommandText = sqlQuery;
+			IDataReader reader = dbcmd.ExecuteReader ();
+			while (reader.Read ()) {
+				card_id = reader.GetInt32 (0);
+				card_name = reader.GetString (1);
+				card_image = reader.GetString (2);
+				card_type = reader.GetInt32 (3);
 
-		reader.Close();
-		reader = null;
-		dbcmd.Dispose();
-		dbcmd = null;
-		dbconn.Close();
-		dbconn = null;
+				Debug.Log ("card_id=" + card_id + " card_name=" + card_name + " card_image=" + card_image + " card_type=" + card_type);
+			}
+
+			reader.Close ();
+			reader = null;
+			dbcmd.Dispose ();
+			dbcmd = null;
+			dbconn.Close ();
+			dbconn = null;
+
+		} while ((card_id == 43 || card_id == 44 || card_id == 46 || card_id == 47 || card_id == 51 || card_id == 50 || card_id == 7) && id == 0);
+
 
 		card_t card = new card_t();
 
